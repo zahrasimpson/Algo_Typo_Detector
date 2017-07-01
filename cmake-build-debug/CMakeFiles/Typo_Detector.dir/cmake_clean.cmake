c2564de35fcd9f9d11265be6072cc7e3416c5eff
Bhavika Shah <Bhavika Shah>
2017-07-01 12:50:26 -0400

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Typo_Detector.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Typo_Detector.dir/main.cpp.obj"
+  "Typo_Detector.pdb"
+  "Typo_Detector.exe"
+  "Typo_Detector.exe.manifest"
+  "libTypo_Detector.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Typo_Detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
